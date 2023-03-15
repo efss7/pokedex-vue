bed6655dd524cccf688d139ba78c6b7921029cfb
efss7 <ericfelipess07@gmail.com>
2023-03-15 12:01:46 -0300

    Criando lista de cards pokemon com bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { onMounted, reactive, ref } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue';
+
+let urlBaseSvg = ref('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/')
 let pokemons = reactive(ref());
 
 onMounted(() => {
@@ -17,24 +19,27 @@ onMounted(() => {
       <div class="row mt-4">
 
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
+          <!-- <div class="card" style="width: 18rem;">
             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                 content.</p>
             </div>
-          </div>
+          </div> -->
 
         </div>
 
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <ListPokemons 
-            v-for="pokemon in pokemons"
-            :key="pokemon.name"
-            :name = "pokemon.name"
-            />
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemons 
+              v-for="pokemon in pokemons"
+              :key="pokemon.name"
+              :name = "pokemon.name"
+              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+              />
+            </div>
           </div>
         </div>
 
