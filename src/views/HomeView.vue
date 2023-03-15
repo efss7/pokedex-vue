cad04116078baf556d137263467e706e298b264d
efss7 <ericfelipess07@gmail.com>
2023-03-15 11:17:11 -0300

    Usando fetch para consumir a Pokeapi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,13 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+
+let pokemons = reactive(ref());
+
+onMounted(() => {
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    .then(res => res.json())
+    .then(res => pokemons.value = res.results);
+})
 </script>
 
 <template>
@@ -9,7 +18,7 @@
 
         <div class="col-sm-12 col-md-6">
           <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -19,7 +28,15 @@
 
         </div>
 
-        <div class="col-sm-12 col-md-6">oi</div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <ul>
+              <li v-for="pokemon in pokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+              </li>
+            </ul>
+          </div>
+        </div>
 
       </div>
     </div>
